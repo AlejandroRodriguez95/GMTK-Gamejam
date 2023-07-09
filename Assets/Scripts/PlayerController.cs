f2a25750f55024c2abce360e18b2cdad54efd31c
EricThunderSnake <edvardasm@icloud.com>
2023-07-09 09:57:14 +0100

    Just Pushing

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,13 +11,11 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField]
     SO_PlayerSettings playerSettings;
-    float rotationSpeed;
-    [SerializeField]float innerAngle;
-    [SerializeField]float outerAngle;
+    [SerializeField] float rotationSpeed;
+    [SerializeField] float innerAngle;
+    [SerializeField] float outerAngle;
     // Raise Modifier = +-1 and is used to make sure the active arm always moves upward when scrolling up
     float raiseModifier;
-    Quaternion leftIdleRotation;
-    Quaternion rightIdleRotation;
     [SerializeField]
     Vector3 currentRotation;
   
@@ -27,50 +25,57 @@ public class PlayerController : MonoBehaviour
         this.rotationSpeed = playerSettings.rotationSpeed;
         this.outerAngle = playerSettings.outerAngle;
         this.innerAngle = playerSettings.innerAngle;
+<<<<<<< Updated upstream
         leftIdleRotation = shoulders[0].transform.rotation;
         rightIdleRotation = shoulders[1].transform.rotation;
+=======
+>>>>>>> Stashed changes
         activeShoulder = shoulders[0];
-        activeShoulder.transform.rotation = Quaternion.Euler(currentRotation);
+        activeShoulder.transform.localRotation = Quaternion.Euler(currentRotation);
         raiseModifier = 1;
     }
-    void FixedUpdate()
-    {
 
-    }
     void Update()     
     {
         SelectArm();
+        RenderActiveArm();
         // Moves the selected arm
+<<<<<<< Updated upstream
 
         shoulders[0].transform.rotation = Quaternion.Lerp(shoulders[0].transform.rotation, leftIdleRotation, 3f*Time.deltaTime);
         shoulders[1].transform.rotation = Quaternion.Lerp(shoulders[1].transform.rotation, rightIdleRotation, 3f*Time.deltaTime);
 
         if (Input.GetKey(KeyCode.Space)){
             activeShoulder.transform.Rotate(rotationSpeed * raiseModifier * Vector3.forward);
-            currentRotation = activeShoulder.transform.rotation.eulerAngles;
-            if (activeShoulder == shoulders[0])
-            {
-                ArmInContactWithFloor.LeftArmIsInContactWithFloor = false;
-                currentRotation.z = Mathf.Clamp(currentRotation.z, outerAngle, innerAngle);
-            }
+=======
 
+            activeShoulder.transform.Rotate(-rotationSpeed * raiseModifier * Input.GetAxis("Mouse ScrollWheel") * Vector3.forward);
+>>>>>>> Stashed changes
+            currentRotation = activeShoulder.transform.rotation.eulerAngles;
+        if(activeShoulder == shoulders[0])
+            currentRotation.z = Mathf.Clamp(currentRotation.z, outerAngle, innerAngle);
+
+<<<<<<< Updated upstream
             if (activeShoulder == shoulders[1])
             {
                 ArmInContactWithFloor.RightArmIsInContactWithFloor = false;
                 currentRotation.z = Mathf.Clamp(currentRotation.z, 360 - innerAngle, 360 - outerAngle);
             }
+=======
+       /* if (activeShoulder == shoulders[1])
+        {
+            if (currentRotation.z < 180 - innerAngle || currentRotation.z > 180)
+                currentRotation.z = 180 - innerAngle;
+>>>>>>> Stashed changes
 
-            activeShoulder.transform.rotation = Quaternion.Euler(currentRotation);
+            if (currentRotation.z > 180 - outerAngle)
+                currentRotation.z = 180 - outerAngle;
 
-        }
+        }*/
 
-        if (shoulders[0].transform.rotation == leftIdleRotation && ArmInContactWithFloor.LeftArmIsInContactWithFloor == false)
-            ArmInContactWithFloor.LeftArmIsInContactWithFloor = true;
+        activeShoulder.transform.localRotation = Quaternion.Euler(currentRotation);
 
-        if (shoulders[1].transform.rotation == rightIdleRotation && ArmInContactWithFloor.RightArmIsInContactWithFloor == false)
-            ArmInContactWithFloor.RightArmIsInContactWithFloor = true;
-
-
+        
     }
 
     int Heavi(float x)
@@ -102,5 +107,26 @@ public class PlayerController : MonoBehaviour
             raiseModifier = -1;
         }
     }
-   
+    void RenderActiveArm()
+    {
+        foreach (GameObject shoulder in shoulders)
+        {
+            renderers = shoulder.GetComponentsInChildren<SpriteRenderer>();
+            if (shoulder == activeShoulder)
+            {
+                foreach (SpriteRenderer ren in renderers)
+                {
+                    ren.color = Color.red;
+                }
+            }
+            else
+            {
+                foreach (SpriteRenderer ren in renderers)
+                {
+                    ren.color = Color.grey;
+                }
+            }
+
+        }
+    }
 }
